d666130a7d7e259e05e267d957f5ee6e57ecb2d7
Eric Kunze <eric.kunze@gmx.net>
2015-10-25 16:45:29 +0100

    Datenbankzugriff mittels postgresql

---
+++ iron/postgresql/src/main.rs
@@ -0,0 +1,37 @@
+extern crate postgres;
+
+use postgres::{Connection, SslMode};
+
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>
+}
+
+fn main() {
+    let conn = Connection::connect("postgres://testuser:test@localhost/testdb", &SslMode::None)
+            .unwrap();
+
+    conn.execute("CREATE TABLE person (
+                    id              SERIAL PRIMARY KEY,
+                    name            VARCHAR NOT NULL,
+                    data            BYTEA
+                  )", &[]).unwrap();
+    let me = Person {
+        id: 0,
+        name: "Steven".to_string(),
+        data: None
+    };
+    conn.execute("INSERT INTO person (name, data) VALUES ($1, $2)",
+                 &[&me.name, &me.data]).unwrap();
+
+    let stmt = conn.prepare("SELECT id, name, data FROM person").unwrap();
+    for row in stmt.query(&[]).unwrap() {
+        let person = Person {
+            id: row.get(0),
+            name: row.get(1),
+            data: row.get(2)
+        };
+        println!("Found person {}", person.name);
+    }
+}
