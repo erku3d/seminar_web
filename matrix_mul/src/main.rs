197248d1d34e6bdffec094684922c7b25a6bdad3
Eric Kunze <Eric.Kunze@gmx.net>
2015-10-30 08:40:50 +0100

    copy paste bodyparser

--- matrix_mul/src/main.rs
+++ matrix_mul/src/main.rs
@@ -1,3 +1,37 @@
+extern crate iron;
+extern crate bodyparser;
+extern crate persistent;
+extern crate rustc_serialize;
+
+
+use persistent::Read;
+use iron::status;
+use iron::prelude::*;
+
+#[derive(Debug, Clone, RustcDecodable)]
+struct MyStructure {
+    age: Vec<Vec<i32>>,
+    name: Option<String>,
+}
+
+fn log_body(req: &mut Request) -> IronResult<Response> {
+ 
+    let struct_body = req.get::<bodyparser::Struct<MyStructure>>();
+    match struct_body {
+        Ok(Some(struct_body)) => println!("Parsed body:\n{:?} {:?}", struct_body, struct_body.age[0][1]),
+        Ok(None) => println!("No body"),
+        Err(err) => println!("Error: {:?}", err)
+    }
+
+    Ok(Response::with(status::Ok))
+}
+
+const MAX_BODY_LENGTH: usize = 1024 * 1024 * 10;
+
+//curl -i "localhost:3000/" -H "application/json" -d '{"name":"jason","age": [["1","2","3"],["1","2","3"]]}'
+// and check out the printed json in your terminal.
 fn main() {
-    println!("Hello, world!");
+    let mut chain = Chain::new(log_body);
+    chain.link_before(Read::<bodyparser::MaxBodyLength>::one(MAX_BODY_LENGTH));
+    Iron::new(chain).http("localhost:3000").unwrap();
 }
